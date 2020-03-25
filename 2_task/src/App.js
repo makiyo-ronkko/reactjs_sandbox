cc09c1e5e90d10d5fed71506dd51e958b65f529e
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-03-25 14:27:50 +0200

    class component added

--- 2_task/src/App.js
+++ 2_task/src/App.js
@@ -1,21 +1,33 @@
-import React from 'react'; //React component, 'react' module
+import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'; //Person component from js file
+import Person from './Person/Person';
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-      <p>Hellow, again!</p>
-      <Person name="Margit" age="39" />
-      <Person name="Mark" age="35" />
-      <Person name="Jane" age="29" />
-      <Person name="James" age="19" />
-      <Person name="Tom" age="69" />
-      <Person name="Tommy" age="59" />
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Margit', age: 39 },
+      { name: 'Mark', age: 35 },
+      { name: 'Jane', age: 29 },
+      { name: 'James', age: 19 },
+      { name: 'Tom', age: 69 },
+      { name: 'Tommy', age: 59 }
+    ]
+  };
 
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+        <p>Hellow, again!</p>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age} />
+        <Person name={this.state.persons[4].name} age={this.state.persons[4].age} />
+        <Person name={this.state.persons[5].name} age={this.state.persons[5].age} />
+      </div>
+    );
+  }
+}
 
 export default App;
