5616dd86b8967a4854cc973cb5b2231939e160f8
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-03-25 14:01:07 +0200

    added props

--- 2_task/src/App.js
+++ 2_task/src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React from 'react'; //React component, 'react' module
 import './App.css';
-import Person from './Person/Person';
+import Person from './Person/Person'; //Person component from js file
 
 const App = () => {
   return (
     <div>
       <h1>Hello, World!</h1>
       <p>Hellow, again!</p>
-      <Person />
+      <Person name="Margit" age="39" />
+      <Person name="Mark" age="35" />
+      <Person name="Jane" age="29" />
+      <Person name="James" age="19" />
+      <Person name="Tom" age="69" />
+      <Person name="Tommy" age="59" />
     </div>
   );
 };
--- 2_task/src/Person/Person.js
+++ 2_task/src/Person/Person.js
@@ -1,8 +1,8 @@
 import React from 'react'; //calling react from node_module
 import './Person.css';
 
-const Person = () => {
-    return (<div class="card"><p>Hello, my name is Margit and I am 39 years old.</p></div>);
+const Person = (props) => {
+    return (<div class="card"><p>Hello, my name is {props.name} and I am {props.age} years old.</p></div>);
 };
 
 export default Person;
