32e6976c21cbed18a83b17f717171d67bb2dc15f
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-03-25 13:23:41 +0200

    created person component

--- 2_task/src/App.js
+++ 2_task/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 const App = () => {
   return (
     <div>
       <h1>Hello, World!</h1>
       <p>Hellow, again!</p>
+      <Person />
     </div>
   );
 };
---
+++ 2_task/src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react'; //calling react from node_module
+import './Person.css';
+
+const Person = () => {
+    return (<div class="card"><p>Hello, my name is Margit and I am 39 years old.</p></div>);
+};
+
+export default Person;
