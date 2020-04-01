32a330311540b1c4cf99db2fb60e5c9d51d4b193
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-04-01 14:41:21 +0300

    adding codes back which accidentaly removed

--- 3_task/src/App.js
+++ 3_task/src/App.js
@@ -1,30 +1,49 @@
-app.get('/helsinki-events-selected', function (req, res) {
-    http.get('http://open-api.myhelsinki.fi/v1/activities/', function (resp) {
-        let data = '';
+import React, { Component } from 'react';
+import './App.css';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
 
-        // A chunk of data has been recieved.
-        resp.on('data', function (chunk) {
-            data += chunk;
-        });
+class App extends Component {
+    state = {
+        likes: 0
+    };
 
-        var info_url = data.info_url;
+    addHandler = () => {
+        console.log('addHandler clicked');
+        this.setState(
+            { likes: this.state.likes + 1 });
 
-        // The whole response has been received. Print out the result.
-        resp.on('end', function () {
-            data = JSON.parse(data);
+    }
+    removeHandler = () => {
+        console.log('removeHandler clicked');
+        this.setState(
+            { likes: this.state.likes - 1 });
+    }
+    resetHandler = () => {
+        console.log('resetHandler clicked');
+        this.setState(
+            { likes: 0 }); // this.state.likes - this.state.likes
+    }
 
-            for (var i = 0, length = data.length; i < length; i++) {
-                res.send(info_url)
-            }
+    render() {
+        return (
+            <div>
+                <Header />
+                <main>
+                    <h1 className={this.state.likes === 0 ? 'likes' : this.state.likes % 2 === 0 ? 'likes even' : 'likes odd'}>Total Likes: {this.state.likes}</h1>
+                </main>
+                <section>
+                    <button onClick={this.addHandler}>Add like</button>
+                    <button onClick={this.removeHandler}>Remove like</button>
+                    <button onClick={this.resetHandler}>Reset likes</button>
+                </section>
+                <Footer />
+            </div>
+        )
+    }
+}
 
-            res.send(info_url)
-            res.send(data);
-            res.end();
+export default App;
 
-        });
 
-    }).on("error", function (err) {
-        console.log("Error: " + err.message);
-        res.end();
-    });
-});
+//(condition) ? (true block) : (else block)
