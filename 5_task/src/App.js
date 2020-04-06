3956b6e14f8464aff0f850ae666c9cffb1f82736
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-04-06 14:54:40 +0300

    Random number finding, start and end game functions, conditional rendering with active added

--- 5_task/src/App.js
+++ 5_task/src/App.js
@@ -2,18 +2,57 @@ import React, { Component } from 'react'; //rcc
 import './App.css';
 import Circle from './Circle/Circle';
 
+const getRndInteger = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + 1) + min;
+};
+
 class App extends Component {
 
   state = {
     score: 0,
+    current: 0,
   };
+
+  // place variables higher as possible
+  pace = 1000;
+  timer = undefined;
+
+  // next function is finding a new number
+  next = () => {
+    let nextActive = undefined; //initially undecided
+
+    do {
+      nextActive = getRndInteger(1, 4);
+    } while (nextActive === this.state.current) //get a new random number until next active and current numbers are same
+
+    this.setState({ // current becomes nextActive number
+      current: nextActive, // keep looping until the game ends
+    });
+
+    this.timer = setTimeout(this.next, this.pace); //two properties, which function, which speed
+    console.log('this is active circle', this.state.current);
+  };
+
+  // finding a random number
+
   //(circleId) is parameter, 1 2 3 and 4.
   clickHandler = (circleId) => {
     console.log('Clicked!', circleId);
     this.setState({
       score: this.state.score + 1
-    })
-  }
+    });
+  };
+
+  // starting the game and running the next() function
+
+  startHandler = () => {
+    this.next();
+  };
+
+  // ending the game by using clearTimeout()
+  endHandler = () => {
+    clearTimeout(this.timer);
+  };
 
   render() {
     return (
@@ -25,15 +64,15 @@ class App extends Component {
         </div>
 
         <main>
-          <Circle click={this.clickHandler.bind(this, 1)} />
-          <Circle click={this.clickHandler.bind(this, 2)} />
-          <Circle click={this.clickHandler.bind(this, 3)} />
-          <Circle click={this.clickHandler.bind(this, 4)} />
+          <Circle active={this.state.current === 1} click={this.clickHandler.bind(this, 1)} defaultColor='yellow' />
+          <Circle active={this.state.current === 2} click={this.clickHandler.bind(this, 2)} defaultColor='green' />
+          <Circle active={this.state.current === 3} click={this.clickHandler.bind(this, 3)} defaultColor='red' />
+          <Circle active={this.state.current === 4} click={this.clickHandler.bind(this, 4)} defaultColor='blue' />
         </main>
 
         <div className='button-container'>
-          <button>Start Game</button>
-          <button>End Game</button>
+          <button onClick={this.startHandler}>Start Game</button>
+          <button onClick={this.endHandler}>End Game</button>
         </div>
 
       </div >
--- 5_task/src/Circle/Circle.js
+++ 5_task/src/Circle/Circle.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './Circle.css';
 
-const Circle = props => {
+const Circle = (props) => {
     return (
-        <div className="circle" onClick={props.click}>
-
+        <div className={'circle' + (props.active ? ' active' : ' ')}
+            onClick={props.click}
+            style={{
+                backgroundColor: props.active ? props.active : props.defaultColor,
+            }}
+        >
         </div>
     )
 }
-
+// conditional rendering = whether active or not
+//if props.active is true, props.active executed otherwise buttonColor
 
 export default Circle;
