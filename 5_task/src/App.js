1299e2a09eff5d76101a6b415a91878f85f4ce3a
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-04-06 13:32:52 +0300

    git Circles and clickHandler added

--- 5_task/src/App.js
+++ 5_task/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'; //rcc
 import './App.css';
+import Circle from './Circle/Circle';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    score: 0,
+  };
+  //(circleId) is parameter, 1 2 3 and 4.
+  clickHandler = (circleId) => {
+    console.log('Clicked!', circleId);
+    this.setState({
+      score: this.state.score + 1
+    })
+  }
+
+  render() {
+    return (
+      <div>
+
+        <div className='header-container'>
+          <h1>Speed Game</h1>
+          <p>Your score: {this.state.score}</p>
+        </div>
+
+        <main>
+          <Circle click={this.clickHandler.bind(this, 1)} />
+          <Circle click={this.clickHandler.bind(this, 2)} />
+          <Circle click={this.clickHandler.bind(this, 3)} />
+          <Circle click={this.clickHandler.bind(this, 4)} />
+        </main>
+
+        <div className='button-container'>
+          <button>Start Game</button>
+          <button>End Game</button>
+        </div>
+
+      </div >
+    )
+  }
+
+
+
 }
 
 export default App;
---
+++ 5_task/src/Circle/Circle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Circle.css';
+
+const Circle = props => {
+    return (
+        <div className="circle" onClick={props.click}>
+
+        </div>
+    )
+}
+
+
+export default Circle;
