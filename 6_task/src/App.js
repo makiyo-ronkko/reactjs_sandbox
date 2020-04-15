6f7a95cccd9cfc7f51b38e0532b4a054fa0ea7d2
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-04-15 14:22:39 +0300

    posts are created dynamically from the array and removed from the array with click event.

--- 6_task/src/App.js
+++ 6_task/src/App.js
@@ -1,16 +1,57 @@
 import React, { Component } from 'react';
-import Lecture from './Lecture/Lecture';
-import MapMethod from './Lecture/MapMethod';
+import Post from './Post/Post';
+
+const posts = [{
+  id: 1,
+  img: "https://source.unsplash.com/daily",
+  title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  author: "John Smith",
+  desc: "Lorem ipsum dolor sit amet, nec probo eripuit propriae no, mucius appareat moderatius id duo, duo mazim fabellas ea. Vel porro perfecto ne, omnesque disputationi ex duo. Ex illum elitr ocurreret sit. Pro ei nemore omittantur voluptatibus, dicit graeco ut pri. Eu eum duis populo discere.",
+},
+{
+  id: 2,
+  img: "https://source.unsplash.com/1600x900/?nature,water",
+  title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  author: "John Smith",
+  desc: "Lorem ipsum dolor sit amet, nec probo eripuit propriae no, mucius appareat moderatius id duo, duo mazim fabellas ea. Vel porro perfecto ne, omnesque disputationi ex duo. Ex illum elitr ocurreret sit. Pro ei nemore omittantur voluptatibus, dicit graeco ut pri. Eu eum duis populo discere.",
+},
+{
+  id: 3,
+  img: "https://source.unsplash.com/1600x900/?city",
+  title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  author: "John Smith",
+  desc: "Lorem ipsum dolor sit amet, nec probo eripuit propriae no, mucius appareat moderatius id duo, duo mazim fabellas ea. Vel porro perfecto ne, omnesque disputationi ex duo. Ex illum elitr ocurreret sit. Pro ei nemore omittantur voluptatibus, dicit graeco ut pri. Eu eum duis populo discere.",
+},
+];
 
 class App extends Component {
+  state = {
+    posts: posts,
+  };
+
+  removeHandler = (removeId) => {
+    const oldArray = [...this.state.posts];
+    oldArray.splice(removeId, 1);
+    this.setState({ posts: oldArray });
+    console.log('clicked');
+  }
+
   render() {
+
+    //mapping array
+    const postsList = this.state.posts.map((post, index) => { // each element takes the parameter post
+      return (
+        <Post key={post.id} img={post.img} title={post.title} author={post.author} desc={post.desc} click={this.removeHandler.bind(this, index)} />
+      )
+    })
+
+
     return (
-      <div>
-        Hello!
-        <hr />
-        <Lecture />
-        <hr />
-        <MapMethod />
+      <div className="cards" >
+        {postsList}
+        {/* <Post img={this.state.posts[0].img} title={this.state.posts[0].title} author={this.state.posts[0].author} desc={this.state.posts[0].desc} />
+        <Post img={this.state.posts[1].img} title={this.state.posts[1].title} author={this.state.posts[1].author} desc={this.state.posts[1].desc} />
+        <Post img={this.state.posts[2].img} title={this.state.posts[2].title} author={this.state.posts[2].author} desc={this.state.posts[2].desc} /> */}
       </div>
     );
   }
---
+++ 6_task/src/Post/Post.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Post.css';
+
+
+const Post = (props) => {
+
+    return (
+        <div className="card-container" onClick={props.click}>
+            <img src={props.img} alt={props.title} />
+            <h2>{props.title}</h2>
+            <p>{props.author}</p>
+            <p>{props.desc}</p>
+        </div>
+    );
+};
+
+export default Post;
