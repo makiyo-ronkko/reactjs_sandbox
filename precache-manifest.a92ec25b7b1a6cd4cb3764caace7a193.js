f31af859591778b07e95d9386f31199c270487a4
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-05-18 12:09:05 +0300

    Updates

---
+++ precache-manifest.a92ec25b7b1a6cd4cb3764caace7a193.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04cb13e35af4582548a08b5e044135db",
+    "url": "/10_task/index.html"
+  },
+  {
+    "revision": "8075bc971f7bb087d72c",
+    "url": "/10_task/static/css/main.af34d16a.chunk.css"
+  },
+  {
+    "revision": "5af304ae1bf0cd8574d7",
+    "url": "/10_task/static/js/2.f2edca4d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/10_task/static/js/2.f2edca4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8075bc971f7bb087d72c",
+    "url": "/10_task/static/js/main.4725c362.chunk.js"
+  },
+  {
+    "revision": "fb2e4eeacd98a36a84c6",
+    "url": "/10_task/static/js/runtime-main.307ed12a.js"
+  }
+]);
