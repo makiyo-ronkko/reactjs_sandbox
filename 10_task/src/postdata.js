0398bdab5ead3e982ceaa7b69c3faa6d31707f4c
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-05-18 16:27:00 +0300

    Update postdata

--- 10_task/src/postdata.js
+++ 10_task/src/postdata.js
@@ -1,37 +1,37 @@
 const postdata = [
   {
     id: 1,
-    title: "Post1",
+    title: "About Sky",
     desc:
       "Morbi sed aliquet nisi, in efficitur risus. Donec porttitor facilisis pulvinar. Maecenas porta eros eget dui ultrices sollicitudin. Vestibulum suscipit, libero eu vehicula cursus, metus ante vestibulum nisi, sed sollicitudin nisl ex ac arcu. ",
     img: "https://source.unsplash.com/1600x900/?sky",
   },
   {
     id: 2,
-    title: "Post2",
+    title: "About Forest",
     desc:
-      "Morbi non venenatis diam. Praesent ante diam, efficitur sit amet dui at, ultrices venenatis ex. Mauris tristique, nunc eu volutpat pulvinar, magna neque tempor quam, vel sodales dolor risus at massa.",
+      "Morbi sed aliquet nisi, in efficitur risus. Donec porttitor facilisis pulvinar. Maecenas porta eros eget dui ultrices sollicitudin. Vestibulum suscipit, libero eu vehicula cursus, metus ante vestibulum nisi, sed sollicitudin nisl ex ac arcu. ",
     img: "https://source.unsplash.com/1600x900/?forest",
   },
   {
     id: 3,
-    title: "Post3",
+    title: "About Nature",
     desc:
-      "Quisque ut libero porttitor, aliquam felis ac, molestie arcu. Pellentesque sit amet tortor efficitur tellus pharetra ultrices. Quisque quis pretium massa. Duis consectetur porttitor pellentesque.",
+      "Morbi sed aliquet nisi, in efficitur risus. Donec porttitor facilisis pulvinar. Maecenas porta eros eget dui ultrices sollicitudin. Vestibulum suscipit, libero eu vehicula cursus, metus ante vestibulum nisi, sed sollicitudin nisl ex ac arcu. ",
     img: "https://source.unsplash.com/1600x900/?nature",
   },
   {
     id: 4,
-    title: "Post4",
+    title: "About Mountain",
     desc:
-      "Quisque malesuada felis quam, eget egestas nisi pharetra sed. Quisque vulputate mauris placerat eros finibus rhoncus malesuada in tellus. Ut accumsan risus quam, in fermentum lectus suscipit non. Phasellus in semper nisl.",
+      "Morbi sed aliquet nisi, in efficitur risus. Donec porttitor facilisis pulvinar. Maecenas porta eros eget dui ultrices sollicitudin. Vestibulum suscipit, libero eu vehicula cursus, metus ante vestibulum nisi, sed sollicitudin nisl ex ac arcu. ",
     img: "https://source.unsplash.com/1600x900/?mountain",
   },
   {
     id: 5,
-    title: "Post5",
+    title: "About Tree",
     desc:
-      "Proin vehicula, augue et consequat lobortis, enim orci ultricies mauris, in facilisis metus turpis et purus. Integer pretium quis lectus at efficitur. Donec varius a lacus ut iaculis. Quisque porta, magna vitae accumsan rutrum, lacus orci volutpat sem, mattis hendrerit lacus ipsum in sem.",
+      "Morbi sed aliquet nisi, in efficitur risus. Donec porttitor facilisis pulvinar. Maecenas porta eros eget dui ultrices sollicitudin. Vestibulum suscipit, libero eu vehicula cursus, metus ante vestibulum nisi, sed sollicitudin nisl ex ac arcu. ",
     img: "https://source.unsplash.com/1600x900/?tree",
   },
 ];
