2dbe5667f5584ba7f41c33ccd3569d0019ac1325
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-05-18 16:27:37 +0300

    Delete axios from mood component

---
+++ 10_task/src/moods.js
@@ -0,0 +1,18 @@
+const moodsList = [{
+    id: 1,
+    title: "HAPPY",
+    like: 0
+},
+{
+    id: 2,
+    title: "NOTHING",
+    like: 0
+},
+{
+    id: 3,
+    title: "DEPRESSED",
+    like: 0
+}
+];
+
+export default moodsList;
